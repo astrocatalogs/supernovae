df1f7c8639150711cbf2896141efcdc7a1429f6f
guillochon <guillochon@gmail.com>
2016-07-21 20:12:45 -0400

    MAINT: updated generate-web.sh

--- scripts/generate-web.sh
+++ scripts/generate-web.sh
@@ -4,13 +4,12 @@ set -e
 PATH=/opt/local/bin:/usr/local/bin:$PATH ; export PATH
 LD_LIBRARY_PATH=/usr/local/lib:/opt/local/lib ; export LD_LIBRARY_PATH
 
-cd /var/www/html/sne/sne/scripts
-./make-catalog.py &
-./make-catalog.py -by &
-./find-dupes.py &
-./find-conflicts.py &
-./make-biblio.py &
-./make-errata.py &
-./make-host-catalog.py &
-./hammertime.py &
-./make-histograms.py &
+cd /var/www/html/sne/astrocats
+python3.5 -m astrocats.supernovae.scripts.webcat &
+python3.5 -m astrocats.supernovae.scripts.dupecat &
+python3.5 -m astrocats.supernovae.scripts.conflictcat &
+python3.5 -m astrocats.supernovae.scripts.bibliocat &
+python3.5 -m astrocats.supernovae.scripts.erratacat &
+python3.5 -m astrocats.supernovae.scripts.hostcat &
+python3.5 -m astrocats.supernovae.scripts.hammertime &
+python3.5 -m astrocats.supernovae.scripts.histograms &
