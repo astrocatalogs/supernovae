090b2891ade3c1ee35aeec02a4132322ff777698
guillochon <guillochon@gmail.com>
2016-08-01 18:41:21 -0700

    Added WISeREP private count script.

--- scripts/privatecount.py
+++ scripts/privatecount.py
@@ -2,18 +2,33 @@
 import json
 import os
 import re
+from tqdm import tqdm
 from glob import glob
 
 totcount = 0
 totcountp14 = 0
 evcount = 0
-for folder in glob('../input/sne-external-WISEREP/*'):
+with open('../output/catalog.min.json', 'r') as f:
+    filetext = f.read()
+    meta = json.loads(filetext)
+
+metaals = [[y['value'].upper() for y in x['alias']] for x in meta]
+
+names = []
+
+for folder in tqdm(glob('../input/sne-external-WISEREP/*')):
     path = folder + '/README.json'
     name = folder.split('/')[-1]
     if os.path.isfile(path):
         with open(path, 'r') as f:
             dat = json.loads(f.read())
-            if "Private Spectra" in dat and 'PTF' in name.upper():
+            aliases = []
+            for al in metaals:
+                if name.upper() in al:
+                    aliases = al
+            if not aliases:
+                continue
+            if "Private Spectra" in dat and any([x for x in aliases]):
                 nums = re.findall(r'\d+', name)
                 if nums:
                     year = int('20' + nums[0])
@@ -25,6 +40,7 @@ for folder in glob('../input/sne-external-WISEREP/*'):
                     totcount += privcount
                     if year < 2014:
                         totcountp14 += privcount
-                    print(name)
+                        names.append(name)
+                        tqdm.write(name)
 
 print(evcount, totcount, totcountp14)
