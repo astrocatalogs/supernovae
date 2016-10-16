26e68118c0c74e6f49a12cbe13325518f46096d5
guillochon <guillochon@gmail.com>
2016-10-16 12:48:45 -0700

    MAINT: updating web outputs

--- scripts/generate-web.sh
+++ scripts/generate-web.sh
@@ -5,7 +5,7 @@ PATH=/opt/local/bin:/usr/local/bin:$PATH ; export PATH
 LD_LIBRARY_PATH=/usr/local/lib:/opt/local/lib ; export LD_LIBRARY_PATH
 
 cd /var/www/html/sne/astrocats
-python3.5 -m astrocats.supernovae.scripts.webcat &
+python3.5 -m astrocats.scripts.webcat -c sne &
 python3.5 -m astrocats.supernovae.scripts.dupecat &
 python3.5 -m astrocats.supernovae.scripts.conflictcat &
 python3.5 -m astrocats.supernovae.scripts.bibliocat &
