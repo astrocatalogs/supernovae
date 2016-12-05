51022a168f2b1002efa75f54d0eb4827d89895f4
guillochon <guillochon@gmail.com>
2016-12-05 00:26:49 -0500

    MAINT: cleaned up SCP naming rules

--- utils/clean.py
+++ utils/clean.py
@@ -1,8 +1,10 @@
 '''Clean various supernova-specific values.
 '''
-from astrocats.catalog.utils import is_number, zpad, get_sig_digits, pretty_num
-from cdecimal import Decimal
 from math import floor
+
+from astrocats.catalog.utils import (get_sig_digits, is_integer, is_number,
+                                     pretty_num, zpad)
+from cdecimal import Decimal
 
 __all__ = ['name_clean', 'host_clean', 'radec_clean', 'clean_snname']
 
@@ -17,8 +19,8 @@ def name_clean(name):
         newname = newname.replace(':SN', '')
     if newname.startswith('MASJ'):
         newname = newname.replace('MASJ', 'MASTER OT J', 1)
-    if (newname.startswith('MASTER') and
-            len(newname) > 7 and is_number(newname[7])):
+    if (newname.startswith('MASTER') and len(newname) > 7 and
+            is_number(newname[7])):
         newname = newname.replace('MASTER', 'MASTER OT J', 1)
     if newname.startswith('MASTER OT J '):
         newname = newname.replace('MASTER OT J ', 'MASTER OT J', 1)
@@ -30,7 +32,7 @@ def name_clean(name):
                 is_number(namesp[1]) and is_number(namesp[3])):
             newname = 'OGLE-' + namesp[1] + '-SN-' + namesp[3].zfill(3)
         elif (len(namesp) == 2 and is_number(namesp[1][:2]) and
-                not is_number(namesp[1][2:])):
+              not is_number(namesp[1][2:])):
             newname = 'OGLE' + namesp[1]
     if newname.startswith('SN SDSS'):
         newname = newname.replace('SN SDSS ', 'SDSS', 1)
@@ -105,6 +107,8 @@ def name_clean(name):
         newname = newname.replace('SCP', 'SCP-', 1)
     if newname.startswith('SCP- '):
         newname = newname.replace('SCP- ', 'SCP-', 1)
+    if newname.startswith('SCP-') and is_integer(newname[7:]):
+        newname = 'SCP-' + newname[4:7] + str(int(newname[7:]))
     if newname.startswith('PS 1'):
         newname = newname.replace('PS 1', 'PS1', 1)
     if newname.startswith('PS1 SN PS'):
@@ -175,47 +179,44 @@ def name_clean(name):
         newname = newname.replace('snf', 'SNF', 1)
     if newname.startswith('SNF '):
         newname = newname.replace('SNF ', 'SNF', 1)
-    if (newname.startswith('SNF') and
-            is_number(newname[3:]) and len(newname) >= 12):
+    if (newname.startswith('SNF') and is_number(newname[3:]) and
+            len(newname) >= 12):
         newname = 'SNF' + newname[3:11] + '-' + newname[11:]
     if newname.startswith(('MASTER OT J', 'ROTSE3 J')):
         prefix = newname.split('J')[0]
         coords = newname.split('J')[-1].strip()
         decsign = '+' if '+' in coords else '-'
         coordsplit = coords.replace('+', '-').split('-')
-        if ('.' not in coordsplit[0] and
-                len(coordsplit[0]) > 6 and '.' not in coordsplit[1] and
-                len(coordsplit[1]) > 6):
-            newname = (prefix + 'J' + coordsplit[0][:6] + '.' +
-                       coordsplit[0][6:] + decsign + coordsplit[1][:6] +
-                       '.' + coordsplit[1][6:])
-    if (newname.startswith('Gaia ') and
-            is_number(newname[3:4]) and len(newname) > 5):
+        if ('.' not in coordsplit[0] and len(coordsplit[0]) > 6 and
+                '.' not in coordsplit[1] and len(coordsplit[1]) > 6):
+            newname = (
+                prefix + 'J' + coordsplit[0][:6] + '.' + coordsplit[0][6:] +
+                decsign + coordsplit[1][:6] + '.' + coordsplit[1][6:])
+    if (newname.startswith('Gaia ') and is_number(newname[3:4]) and
+            len(newname) > 5):
         newname = newname.replace('Gaia ', 'Gaia', 1)
     if len(newname) <= 4 and is_number(newname):
         newname = 'SN' + newname + 'A'
-    if (len(newname) > 4 and is_number(newname[:4]) and not
-            is_number(newname[4:])):
+    if (len(newname) > 4 and is_number(newname[:4]) and
+            not is_number(newname[4:])):
         newname = 'SN' + newname
-    if (newname.startswith('Sn ') and
-            is_number(newname[3:7]) and len(newname) > 7):
+    if (newname.startswith('Sn ') and is_number(newname[3:7]) and
+            len(newname) > 7):
         newname = newname.replace('Sn ', 'SN', 1)
-    if (newname.startswith('sn') and
-            is_number(newname[2:6]) and len(newname) > 6):
+    if (newname.startswith('sn') and is_number(newname[2:6]) and
+            len(newname) > 6):
         newname = newname.replace('sn', 'SN', 1)
-    if (newname.startswith('SN ') and
-            is_number(newname[3:7]) and len(newname) > 7):
+    if (newname.startswith('SN ') and is_number(newname[3:7]) and
+            len(newname) > 7):
         newname = newname.replace('SN ', 'SN', 1)
-    if (newname.startswith('SN') and
-            is_number(newname[2:6]) and len(newname) == 7 and
-            newname[6].islower()):
+    if (newname.startswith('SN') and is_number(newname[2:6]) and
+            len(newname) == 7 and newname[6].islower()):
         newname = 'SN' + newname[2:6] + newname[6].upper()
     elif (newname.startswith('SN') and is_number(newname[2:6]) and
           (len(newname) == 8 or len(newname) == 9) and newname[6:].isupper()):
         newname = 'SN' + newname[2:6] + newname[6:].lower()
-    if (newname.startswith('AT') and
-            is_number(newname[2:6]) and len(newname) == 7 and
-            newname[6].islower()):
+    if (newname.startswith('AT') and is_number(newname[2:6]) and
+            len(newname) == 7 and newname[6].islower()):
         newname = 'AT' + newname[2:6] + newname[6].upper()
     elif (newname.startswith('AT') and is_number(newname[2:6]) and
           (len(newname) == 8 or len(newname) == 9) and newname[6:].isupper()):
@@ -241,7 +242,7 @@ def radec_clean(svalue, quantity, unit=''):
             minutes = 0 if minutes < 1.e-6 else minutes
             seconds = 0.0 if seconds < 1.e-6 else seconds
             if seconds > 60.0:
-                raise(ValueError('Invalid seconds value for ' + quantity))
+                raise (ValueError('Invalid seconds value for ' + quantity))
             svalue = str(hours).zfill(2) + ':' + str(minutes).zfill(2) + \
                 ':' + zpad(pretty_num(seconds, sig=sig - 1))
         elif 'dec' in quantity:
@@ -252,11 +253,12 @@ def radec_clean(svalue, quantity, unit=''):
             minutes = 0 if minutes < 1.e-6 else minutes
             seconds = 0.0 if seconds < 1.e-6 else seconds
             if seconds > 60.0:
-                raise(ValueError('Invalid seconds value for ' + quantity))
-            svalue = (('+' if deg >= 0.0 else '-') +
-                      str(degree).strip('+-').zfill(2) + ':' +
-                      str(minutes).zfill(2) + ':' +
-                      zpad(pretty_num(seconds, sig=sig - 1)))
+                raise (ValueError('Invalid seconds value for ' + quantity))
+            svalue = (
+                ('+' if deg >= 0.0 else '-') + str(degree).strip('+-').zfill(2)
+                + ':' + str(minutes).zfill(2) + ':' +
+                zpad(pretty_num(
+                    seconds, sig=sig - 1)))
     elif unit == 'nospace' and 'ra' in quantity:
         svalue = svalue[:2] + ':' + svalue[2:4] + \
             ((':' + zpad(svalue[4:])) if len(svalue) > 4 else '')
@@ -271,12 +273,12 @@ def radec_clean(svalue, quantity, unit=''):
         svalue = svalue.replace(' ', ':')
         if 'dec' in quantity:
             valuesplit = svalue.split(':')
-            svalue = (('-' if valuesplit[0].startswith('-') else '+') +
-                      valuesplit[0].strip('+-').zfill(2) +
-                      (':' + valuesplit[1].zfill(2) if
-                       len(valuesplit) > 1 else '') +
-                      (':' + zpad(valuesplit[2]) if
-                       len(valuesplit) > 2 else ''))
+            svalue = (('-' if valuesplit[0].startswith('-') else '+'
+                       ) + valuesplit[0].strip('+-').zfill(2) +
+                      (':' + valuesplit[1].zfill(2)
+                       if len(valuesplit) > 1 else '') +
+                      (':' + zpad(valuesplit[2])
+                       if len(valuesplit) > 2 else ''))
 
     if 'ra' in quantity:
         sunit = 'hours'
@@ -286,8 +288,8 @@ def radec_clean(svalue, quantity, unit=''):
     # Correct case of arcseconds = 60.0.
     valuesplit = svalue.split(':')
     if len(valuesplit) == 3 and valuesplit[-1] in ["60.0", "60.", "60"]:
-        svalue = valuesplit[0] + ':' + str(Decimal(valuesplit[1]) +
-                                           Decimal(1.0)) + ':' + "00.0"
+        svalue = valuesplit[0] + ':' + str(
+            Decimal(valuesplit[1]) + Decimal(1.0)) + ':' + "00.0"
 
     # Strip trailing dots.
     svalue = svalue.rstrip('.')
@@ -336,15 +338,13 @@ def host_clean(name):
     if len(newname) > 4 and newname.startswith("UGC "):
         newname = newname[:4] + newname[4:].lstrip(" 0")
     if len(newname) > 5 and newname.startswith(("MCG +", "MCG -")):
-        newname = newname[:5] + '-'.join([x.zfill(2)
-                                          for x in
-                                          newname[5:].strip().split("-")])
+        newname = newname[:5] + '-'.join(
+            [x.zfill(2) for x in newname[5:].strip().split("-")])
     if len(newname) > 5 and newname.startswith("CGCG "):
-        newname = newname[:5] + '-'.join([x.zfill(3)
-                                          for x in
-                                          newname[5:].strip().split("-")])
+        newname = newname[:5] + '-'.join(
+            [x.zfill(3) for x in newname[5:].strip().split("-")])
     if ((len(newname) > 1 and newname.startswith("E")) or
-            (len(newname) > 3 and newname.startswith('ESO'))):
+        (len(newname) > 3 and newname.startswith('ESO'))):
         if newname[0] == "E":
             esplit = newname[1:].split("-")
         else:
