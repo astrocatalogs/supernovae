3656990560aa868f1623d2c6da2ae419f283faaf
guillochon <guillochon@gmail.com>
2017-01-07 23:31:14 -0500

    ENH: can now read more fits spectra

--- tasks/general_data.py
+++ tasks/general_data.py
@@ -5,10 +5,11 @@ import os
 from collections import OrderedDict
 from glob import glob
 
-from astropy.io import fits
-
 from astrocats.catalog.spectrum import SPECTRUM
 from astrocats.catalog.utils import jd_to_mjd, pbar_strings
+from astropy.io import fits
+from astropy.time import Time as astrotime
+
 from cdecimal import Decimal
 
 from ..supernova import SUPERNOVA, Supernova
@@ -95,34 +96,56 @@ def do_external_xray(catalog):
 
 
 def do_external_fits_spectra(catalog):
-    fureps = {
-        'erg/cm2/s/A': 'erg/s/cm^2/Angstrom'
-    }
+    fureps = {'erg/cm2/s/A': 'erg/s/cm^2/Angstrom'}
     task_str = catalog.get_current_task_str()
     path_pattern = os.path.join(catalog.get_current_task_repo(), '*.fits')
     files = glob(path_pattern)
     for datafile in files:
         hdulist = fits.open(datafile)
         filename = datafile.split('/')[-1]
+        for oi, obj in enumerate(hdulist[0].header):
+            if '.' in obj:
+                del (hdulist[0].header[oi])
         hdulist[0].verify('silentfix')
+        hdrkeys = list(hdulist[0].header.keys())
+        # print(hdrkeys)
         name = hdulist[0].header['OBJECT']
-        observer = hdulist[0].header['OBSERVER']
-        name, source = catalog.new_entry(name, srcname=observer)
+        if 'OBSERVER' in hdrkeys:
+            name, source = catalog.new_entry(
+                name, srcname=hdulist[0].header['OBSERVER'])
+        else:
+            name = catalog.add_entry(name)
+            source = catalog.entries[name].add_self_source()
         # for key in hdulist[0].header.keys():
         #     print(key, hdulist[0].header[key])
         if hdulist[0].header['SIMPLE']:
             fluxes = [str(x) for x in list(hdulist[0].data)]
-            mjd = str(jd_to_mjd(Decimal(str(hdulist[0].header['JD']))))
+            if 'JD' in hdrkeys:
+                mjd = str(jd_to_mjd(Decimal(str(hdulist[0].header['JD']))))
+            elif 'MJD' in hdrkeys:
+                mjd = str(hdulist[0].header['MJD'])
+            elif 'DATE-OBS' in hdrkeys:
+                if 'T' in hdulist[0].header['DATE-OBS']:
+                    dateobs = hdulist[0].header['DATE-OBS'].strip()
+                elif 'UTC-OBS' in hdrkeys:
+                    dateobs = hdulist[0].header['DATE-OBS'].strip(
+                    ) + 'T' + hdulist[0].header['UTC-OBS'].strip()
+                mjd = str(astrotime(dateobs, format='isot').mjd)
+            else:
+                raise ValueError("Couldn't find JD/MJD for spectrum.")
             w0 = hdulist[0].header['CRVAL1']
-            wd = hdulist[0].header['CDELT1']
+            if 'CDELT1' in hdrkeys:
+                wd = hdulist[0].header['CDELT1']
+            else:
+                print('Warning: Skipping FITS spectrum `{}`.'.format(filename))
+                continue
             waves = [str(w0 + wd * x) for x in range(0, len(fluxes))]
         else:
             raise ValueError('Non-simple FITS import not yet supported.')
         tel = hdulist[0].header['TELESCOP']
-        observatory = hdulist[0].header['SITENAME']
         inst = hdulist[0].header['INSTRUME']
         airmass = hdulist[0].header['AIRMASS']
-        if 'BUNIT' in hdulist[0].header:
+        if 'BUNIT' in hdrkeys:
             fluxunit = hdulist[0].header['BUNIT']
             if fluxunit in fureps:
                 fluxunit = fureps[fluxunit]
@@ -139,13 +162,17 @@ def do_external_fits_spectra(catalog):
             SPECTRUM.FLUXES: fluxes,
             SPECTRUM.U_FLUXES: fluxunit,
             SPECTRUM.TELESCOPE: tel,
-            SPECTRUM.OBSERVER: observer,
-            SPECTRUM.OBSERVATORY: observatory,
             SPECTRUM.INSTRUMENT: inst,
             SPECTRUM.AIRMASS: airmass,
             SPECTRUM.FILENAME: filename,
             SPECTRUM.SOURCE: source
         }
+        if 'SITENAME' in hdrkeys:
+            specdict[SPECTRUM.OBSERVATORY] = hdulist[0].header['SITENAME']
+        elif 'OBSERVAT' in hdrkeys:
+            specdict[SPECTRUM.OBSERVATORY] = hdulist[0].header['OBSERVAT']
+        if 'OBSERVER' in hdrkeys:
+            specdict[SPECTRUM.OBSERVER] = hdulist[0].header['OBSERVER']
         catalog.entries[name].add_spectrum(**specdict)
         hdulist.close()
 
