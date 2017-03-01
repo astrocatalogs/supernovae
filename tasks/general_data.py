0880846c98f04cb9be295f624a315e8f23243d54
guillochon <guillochon@gmail.com>
2017-02-28 19:08:39 -0500

    BUG: fix attribute name

--- tasks/general_data.py
+++ tasks/general_data.py
@@ -130,7 +130,7 @@ def do_external_fits_spectra(catalog):
                 name, bibcode=metadict[filename]['bibcode'])
         elif 'donator' in metadict[filename]:
             name, source = catalog.new_entry(
-                name, bibcode=metadict[filename]['donator'])
+                name, srcname=metadict[filename]['donator'])
         else:
             if 'OBSERVER' in hdrkeys:
                 name, source = catalog.new_entry(
