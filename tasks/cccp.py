4e3b7d773c4d20f9027c17a29077160ef7a560a2
lzkelley <lkelley@cfa.harvard.edu>
2018-08-30 16:45:06 -0500

    CCCP: Check for empty values, dont add them, cleanup

--- tasks/cccp.py
+++ tasks/cccp.py
@@ -7,17 +7,18 @@ from glob import glob
 from bs4 import BeautifulSoup
 
 from astrocats.utils import pbar
+from astrocats.structures.struct import PHOTOMETRY
 from decimal import Decimal
 
 from ..supernova import SUPERNOVA
+
+WEIZ_URL = 'https://webhome.weizmann.ac.il/home/iair/sc_cccp.html'
 
 
 def do_cccp(catalog):
     task_str = catalog.get_current_task_str()
     cccpbands = ['B', 'V', 'R', 'I']
-    file_names = list(
-        glob(os.path.join(catalog.get_current_task_repo(),
-                          'CCCP/apj407397*.txt')))
+    file_names = list(glob(os.path.join(catalog.get_current_task_repo(), 'CCCP/apj407397*.txt')))
     for datafile in pbar(file_names, task_str + ': apj407397...', sort=True):
         with open(datafile, 'r') as ff:
             tsvin = csv.reader(ff, delimiter='\t', skipinitialspace=True)
@@ -27,42 +28,43 @@ def do_cccp(catalog):
                 elif rr == 1:
                     name = 'SN' + row[0].split('SN ')[-1]
                     name = catalog.add_entry(name)
-                    source = catalog.entries[name].add_source(
-                        bibcode='2012ApJ...744...10K')
-                    catalog.entries[name].add_quantity(
-                        SUPERNOVA.ALIAS, name, source)
+                    source = catalog.entries[name].add_source(bibcode='2012ApJ...744...10K')
+                    catalog.entries[name].add_quantity(SUPERNOVA.ALIAS, name, source)
                 elif rr >= 5:
                     mjd = str(Decimal(row[0]) + 53000)
                     for bb, band in enumerate(cccpbands):
                         if row[2 * bb + 1]:
                             mag = row[2 * bb + 1].strip('>')
                             upl = (not row[2 * bb + 2])
-                            (catalog.entries[name]
-                             .add_photometry(time=mjd, u_time='MJD', band=band,
-                                             magnitude=mag,
-                                             e_magnitude=row[2 * bb + 2],
-                                             upperlimit=upl, source=source))
+                            photo = {
+                                PHOTOMETRY.TIME: mjd,
+                                PHOTOMETRY.U_TIME: 'MJD',
+                                PHOTOMETRY.BAND: band,
+                                PHOTOMETRY.MAGNITUDE: mag,
+                                PHOTOMETRY.UPPERLIMIT: upl,
+                                PHOTOMETRY.SOURCE: source
+                            }
+                            e_mag = row[2 * bb + 2]
+                            if len(e_mag) > 0:
+                                photo[PHOTOMETRY.E_MAGNITUDE] = e_mag
+
+                            catalog.entries[name].add_photometry(**photo)
 
     html = catalog.load_url(
-        'https://webhome.weizmann.ac.il/home/iair/sc_cccp.html',
-        os.path.join(catalog.get_current_task_repo(),
-                     'CCCP/sc_cccp.html'))
+        WEIZ_URL, os.path.join(catalog.get_current_task_repo(), 'CCCP/sc_cccp.html'))
 
     soup = BeautifulSoup(html, 'html5lib')
     links = soup.body.findAll("a")
+    path = os.path.join(catalog.get_current_task_repo(), 'CCCP/')
     for link in pbar(links, task_str + ': links'):
         if 'sc_sn' in link['href']:
             name = catalog.add_entry(link.text.replace(' ', ''))
-            source = (catalog.entries[name]
-                      .add_source(name='CCCP',
-                                  url=('https://webhome.weizmann.ac.il'
-                                       '/home/iair/sc_cccp.html')))
+            source = catalog.entries[name].add_source(name='CCCP', url=WEIZ_URL)
             catalog.entries[name].add_quantity(SUPERNOVA.ALIAS, name, source)
 
             html2 = catalog.load_url(
                 'https://webhome.weizmann.ac.il/home/iair/' + link['href'],
-                os.path.join(catalog.get_current_task_repo(),
-                             'CCCP/') + link['href'].split('/')[-1])
+                path + link['href'].split('/')[-1])
 
             soup2 = BeautifulSoup(html2, 'html5lib')
             links2 = soup2.body.findAll("a")
@@ -73,17 +75,13 @@ def do_cccp(catalog):
                     # Many 404s in photometry, set cache_only = True unless
                     # attempting complete rebuild.
                     html3 = catalog.load_url(
-                        'https://webhome.weizmann.ac.il/home/iair/cccp/' +
-                        link2['href'],
-                        os.path.join(catalog.get_current_task_repo(),
-                                     'CCCP/') + link2['href'].split('/')[-1],
-                        cache_only=True)
+                        'https://webhome.weizmann.ac.il/home/iair/cccp/' + link2['href'],
+                        path + link2['href'].split('/')[-1], cache_only=True)
 
                     if html3 is None:
                         continue
 
-                    table = [[str(Decimal(yy.strip())).rstrip('0') for yy in
-                              xx.split(',')]
+                    table = [[str(Decimal(yy.strip())).rstrip('0') for yy in xx.split(',')]
                              for xx in list(filter(None, html3.split('\n')))]
                     for row in table:
                         catalog.entries[name].add_photometry(
