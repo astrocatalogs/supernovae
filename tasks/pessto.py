ebf4c4f15a2a2f90132c2590528b70f4b6ae6bbd
lzkelley <lkelley@cfa.harvard.edu>
2018-08-30 15:48:31 -0500

    Check for empty values and do not add

--- tasks/pessto.py
+++ tasks/pessto.py
@@ -11,19 +11,16 @@ from ..supernova import SUPERNOVA
 
 def do_pessto(catalog):
     task_str = catalog.get_current_task_str()
-    pessto_path = os.path.join(
-        catalog.get_current_task_repo(), 'PESSTO_MPHOT.csv')
+    pessto_path = os.path.join(catalog.get_current_task_repo(), 'PESSTO_MPHOT.csv')
     tsvin = list(csv.reader(open(pessto_path, 'r'), delimiter=','))
     for ri, row in enumerate(pbar(tsvin, task_str)):
         if ri == 0:
             bands = [xx.split('_')[0] for xx in row[3::2]]
-            systems = [xx.split('_')[1].capitalize().replace(
-                'Ab', 'AB') for xx in row[3::2]]
+            systems = [xx.split('_')[1].capitalize().replace('Ab', 'AB') for xx in row[3::2]]
             continue
         name = row[1]
         name = catalog.add_entry(name)
-        source = catalog.entries[name].add_source(
-            bibcode='2015A&A...579A..40S')
+        source = catalog.entries[name].add_source(bibcode='2015A&A...579A..40S')
         catalog.entries[name].add_quantity(SUPERNOVA.ALIAS, name, source)
         for hi, ci in enumerate(range(3, len(row) - 1, 2)):
             if not row[ci]:
@@ -40,7 +37,6 @@ def do_pessto(catalog):
                 PHOTOMETRY.TIME: row[2],
                 PHOTOMETRY.U_TIME: 'MJD',
                 PHOTOMETRY.MAGNITUDE: row[ci],
-                PHOTOMETRY.E_MAGNITUDE: row[ci + 1],
                 PHOTOMETRY.BAND: bands[hi],
                 PHOTOMETRY.SYSTEM: systems[hi],
                 PHOTOMETRY.BAND_SET: bandset,
@@ -48,6 +44,9 @@ def do_pessto(catalog):
                 PHOTOMETRY.INSTRUMENT: instrument,
                 PHOTOMETRY.SOURCE: source
             }
+            e_mag = row[ci + 1]
+            if len(e_mag) > 0:
+                photodict[PHOTOMETRY.E_MAGNITUDE] = e_mag
             catalog.entries[name].add_photometry(**photodict)
 
     catalog.journal_entries()
