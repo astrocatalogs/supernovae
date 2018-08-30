ba12194961fb79d6f20817f0760ce564fb2bbff3
lzkelley <lkelley@cfa.harvard.edu>
2018-08-30 15:51:41 -0500

    BUG: in scp import

--- tasks/scp.py
+++ tasks/scp.py
@@ -10,28 +10,23 @@ from ..supernova import SUPERNOVA
 
 def do_scp(catalog):
     task_str = catalog.get_current_task_str()
-    tsvin = list(csv.reader(open(
-        os.path.join(catalog.get_current_task_repo(), 'SCP09.csv'), 'r'),
-        delimiter=','))
+    path = os.path.join(catalog.get_current_task_repo(), 'SCP09.csv')
+    tsvin = list(csv.reader(open(path, 'r'), delimiter=','))
+    url = 'http://supernova.lbl.gov/2009ClusterSurvey/'
     for ri, row in enumerate(pbar(tsvin, task_str)):
         if ri == 0:
             continue
         name = row[0].replace('SCP', 'SCP-')
         name = catalog.add_entry(name)
-        source = (catalog.entries[name]
-                  .add_source(name='Supernova Cosmology Project',
-                              url=('http://supernova.lbl.gov/'
-                                   '2009ClusterSurvey/')))
+        source = catalog.entries[name].add_source(name='Supernova Cosmology Project', url=url)
         catalog.entries[name].add_quantity(SUPERNOVA.ALIAS, name, source)
         if row[1]:
             catalog.entries[name].add_quantity(SUPERNOVA.ALIAS, row[1], source)
         if row[2]:
             kind = 'spectroscopic' if row[3] == 'sn' else SUPERNOVA.HOST
-            catalog.entries[name].add_quantity(
-                SUPERNOVA.REDSHIFT, row[2], source, kind=kind)
+            catalog.entries[name].add_quantity(SUPERNOVA.REDSHIFT, row[2], source, kind=kind)
         if row[4]:
-            catalog.entries[name].add_quantity(
-                SUPERNOVA.REDSHIFT, row[2], source, kind='cluster')
+            catalog.entries[name].add_quantity(SUPERNOVA.REDSHIFT, row[4], source, kind='cluster')
         if row[6]:
             claimedtype = row[6].replace('SN ', '')
             kind = ('spectroscopic/light curve' if 'a' in row[7] and 'c' in
