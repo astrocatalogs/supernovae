7658edc559475f64b8b4e0d7727c692abbce95fe
lzkelley <lkelley@cfa.harvard.edu>
2018-08-31 14:57:28 -0500

    SNF_SPECTRA: Check for empty values, dont add them, cleanup

--- tasks/snf.py
+++ tasks/snf.py
@@ -9,12 +9,12 @@ from astropy.time import Time as astrotime
 
 from decimal import Decimal
 
+from astrocats.structures.struct import SPECTRUM
 from ..supernova import SUPERNOVA
 
 
 def do_snf_aliases(catalog):
-    file_path = os.path.join(
-        catalog.get_current_task_repo(), 'SNF/snf-aliases.csv')
+    file_path = os.path.join(catalog.get_current_task_repo(), 'SNF/snf-aliases.csv')
     with open(file_path, 'r') as f:
         for row in [x.split(',') for x in f.read().splitlines()]:
             name, source = catalog.new_entry(
@@ -34,11 +34,13 @@ def do_snf_spectra(catalog):
                 'SN2011fe': '2013A&A...554A..27P'}
     oldname = ''
     snfcnt = 0
-    eventfolders = next(os.walk(os.path.join(
-        catalog.get_current_task_repo(), 'SNFactory')))[1]
+    eventfolders = next(os.walk(os.path.join(catalog.get_current_task_repo(), 'SNFactory')))[1]
     for eventfolder in pbar(eventfolders, task_str):
-        oname = eventfolder
-        name = catalog.get_name_for_entry_or_alias(oname)
+        name = eventfolder
+        # Use existing name if already added
+        _name = catalog.get_name_for_entry_or_alias(name)
+        if _name is not None:
+            name = _name
         if oldname and name != oldname:
             catalog.journal_entries()
         oldname = name
@@ -47,27 +49,24 @@ def do_snf_spectra(catalog):
         sec_refurl = 'http://snfactory.lbl.gov/'
         sec_bibcode = '2002SPIE.4836...61A'
         sec_source = catalog.entries[name].add_source(
-            name=sec_reference, url=sec_refurl, bibcode=sec_bibcode,
-            secondary=True)
-        catalog.entries[name].add_quantity(SUPERNOVA.ALIAS, oname, sec_source)
-        bibcode = bibcodes[oname]
+            name=sec_reference, url=sec_refurl, bibcode=sec_bibcode, secondary=True)
+        catalog.entries[name].add_quantity(SUPERNOVA.ALIAS, name, sec_source)
+        bibcode = bibcodes[name]
         source = catalog.entries[name].add_source(bibcode=bibcode)
         sources = uniq_cdl([source, sec_source])
-        use_path = os.path.join(
-            catalog.get_current_task_repo(), 'SNFactory', eventfolder, '*.dat')
+        use_path = os.path.join(catalog.get_current_task_repo(), 'SNFactory', eventfolder, '*.dat')
         eventspectra = glob(use_path)
         for spectrum in pbar(eventspectra, task_str):
             filename = os.path.basename(spectrum)
             with open(spectrum) as spec_file:
-                specdata = list(csv.reader(
-                    spec_file, delimiter=' ', skipinitialspace=True))
+                specdata = list(csv.reader(spec_file, delimiter=' ', skipinitialspace=True))
             specdata = list(filter(None, specdata))
             newspec = []
-            time = ''
-            telescope = ''
-            instrument = ''
-            observer = ''
-            observatory = ''
+            time = None
+            telescope = None
+            instrument = None
+            observer = None
+            observatory = None
             if 'Keck_20060202_R' in spectrum:
                 time = '53768.23469'
             elif 'Spectrum05_276' in spectrum:
@@ -103,29 +102,43 @@ def do_snf_spectra(catalog):
             if not time:
                 raise ValueError('Time missing from spectrum.')
             specdata = newspec
-            haserrors = len(specdata[0]) == 3 and specdata[
-                0][2] and specdata[0][2] != 'NaN'
+            haserrors = len(specdata[0]) == 3 and specdata[0][2] and specdata[0][2] != 'NaN'
             specdata = [list(i) for i in zip(*specdata)]
 
             wavelengths = specdata[0]
             fluxes = specdata[1]
-            errors = ''
+            errors = None
             if haserrors:
                 errors = specdata[2]
 
-            unit_err = ('Variance' if oldname == 'SN2011fe' else
-                        'erg/s/cm^2/Angstrom')
+            unit_err = 'Variance' if oldname == 'SN2011fe' else 'erg/s/cm^2/Angstrom'
             unit_flx = 'erg/s/cm^2/Angstrom'
-            catalog.entries[name].add_spectrum(
-                u_wavelengths='Angstrom', u_fluxes=unit_flx, u_time='MJD',
-                time=time,
-                wavelengths=wavelengths, fluxes=fluxes, errors=errors,
-                observer=observer, observatory=observatory,
-                telescope=telescope, instrument=instrument, u_errors=unit_err,
-                source=sources, filename=filename)
+            spec = {
+                SPECTRUM.U_WAVELENGTHS: 'Angstrom',
+                SPECTRUM.U_FLUXES: unit_flx,
+                SPECTRUM.U_TIME: 'MJD',
+                SPECTRUM.WAVELENGTHS: wavelengths,
+                SPECTRUM.FLUXES: fluxes,
+                SPECTRUM.U_ERRORS: unit_err,
+                SPECTRUM.SOURCE: sources,
+                SPECTRUM.FILENAME: filename
+            }
+            if errors is not None:
+                spec[SPECTRUM.ERRORS] = errors
+            if time is not None:
+                spec[SPECTRUM.TIME] = time
+            if observer is not None:
+                spec[SPECTRUM.OBSERVER] = observer
+            if observatory is not None:
+                spec[SPECTRUM.OBSERVATORY] = observatory
+            if telescope is not None:
+                spec[SPECTRUM.TELESCOPE] = telescope
+            if instrument is not None:
+                spec[SPECTRUM.INSTRUMENT] = instrument
+            catalog.entries[name].add_spectrum(**spec)
+
             snfcnt = snfcnt + 1
-            if (catalog.args.travis and
-                    snfcnt % catalog.TRAVIS_QUERY_LIMIT == 0):
+            if catalog.args.travis and snfcnt >= catalog.TRAVIS_QUERY_LIMIT:
                 break
 
     catalog.journal_entries()
--- tasks/superfit.py
+++ tasks/superfit.py
@@ -9,14 +9,14 @@ from astropy.time import Time as astrotime
 
 from decimal import Decimal
 
+from astrocats.structures.struct import SPECTRUM
 from ..supernova import SUPERNOVA
 
 
 def do_superfit_spectra(catalog):
     superfit_url = 'http://www.dahowell.com/superfit.html'
     task_str = catalog.get_current_task_str()
-    sfdirs = list(
-        glob(os.path.join(catalog.get_current_task_repo(), 'superfit/*')))
+    sfdirs = list(glob(os.path.join(catalog.get_current_task_repo(), 'superfit/*')))
     for sfdir in pbar(sfdirs, task_str):
         sffiles = sorted(glob(sfdir + '/*.dat'))
         lastname = ''
@@ -34,7 +34,7 @@ def do_superfit_spectra(catalog):
             if 'theory' in name:
                 continue
             prefname = catalog.get_name_for_entry_or_alias(name)
-            if prefname is not None::
+            if prefname is not None:
                 if ('spectra' in catalog.entries[prefname] and lastname != prefname):
                     continue
             if oldname and name != oldname:
@@ -42,8 +42,7 @@ def do_superfit_spectra(catalog):
             oldname = name
             name = catalog.add_entry(name)
             epoch = basename.split('.')[1]
-            mldt, mlmag, mlband, mlsource = \
-                catalog.entries[name]._get_max_light()
+            mldt, mlmag, mlband, mlsource = catalog.entries[name]._get_max_light()
             if mldt:
                 if epoch == 'max':
                     epoff = Decimal(0.0)
@@ -56,34 +55,36 @@ def do_superfit_spectra(catalog):
 
             source = catalog.entries[name].add_source(
                 name='Superfit', url=superfit_url, secondary=True)
-            catalog.entries[name].add_quantity(
-                SUPERNOVA.ALIAS, oldname, source)
+            catalog.entries[name].add_quantity(SUPERNOVA.ALIAS, oldname, source)
 
             with open(sffile) as ff:
                 rows = ff.read().splitlines()
             specdata = []
             for row in rows:
                 if row.strip():
-                    specdata.append(
-                        list(filter(None, re.split('\t+|\s+', row,
-                                                   maxsplit=0))))
-            specdata = [[xx.replace('D', 'E') for xx in list(ii)]
-                        for ii in zip(*specdata)]
+                    specdata.append(list(filter(None, re.split('\t+|\s+', row, maxsplit=0))))
+            specdata = [[xx.replace('D', 'E') for xx in list(ii)] for ii in zip(*specdata)]
             wavelengths = specdata[0]
             fluxes = specdata[1]
 
             if epoff != '':
-                mlmjd = astrotime(
-                    '-'.join([str(mldt.year), str(mldt.month),
-                              str(mldt.day)])).mjd
+                mlmjd = astrotime('-'.join([str(mldt.year), str(mldt.month), str(mldt.day)])).mjd
                 mlmjd = str(Decimal(mlmjd) + epoff)
             else:
-                mlmjd = ''
-            catalog.entries[name].add_spectrum(
-                u_wavelengths='Angstrom', u_fluxes='Uncalibrated',
-                u_time='MJD' if mlmjd else '',
-                time=mlmjd, wavelengths=wavelengths, fluxes=fluxes,
-                source=source, filename=basename)
+                mlmjd = None
+
+            spec = {
+                SPECTRUM.U_WAVELENGTHS: 'Angstrom',
+                SPECTRUM.U_FLUXES: 'Uncalibrated',
+                SPECTRUM.WAVELENGTHS: wavelengths,
+                SPECTRUM.FLUXES: fluxes,
+                SPECTRUM.SOURCE: source,
+                SPECTRUM.FILENAME: basename
+            }
+            if mlmjd:
+                spec[SPECTRUM.TIME] = mlmjd
+                spec[SPECTRUM.U_TIME] = 'MJD'
+            catalog.entries[name].add_spectrum(**spec)
 
             lastname = name
 
