e4125c4abd4148514fa2430381a58e3a3b5d0657
lzkelley <lkelley@cfa.harvard.edu>
2018-08-31 14:03:39 -0500

    SNAX: Check for empty values, dont add them, cleanup

--- tasks/snax.py
+++ tasks/snax.py
@@ -30,41 +30,33 @@ def do_snax(catalog):
     for r, row in enumerate(pbar(data, task_str)):
         if r == 0 or not row[0]:
             continue
-        (name, source) = catalog.new_entry(
-            row[0], srcname='SNaX', url='http://kronos.uchicago.edu/snax/',
-            secondary=True)
+        name, source = catalog.new_entry(
+            row[0], srcname='SNaX', url='http://kronos.uchicago.edu/snax/', secondary=True)
         sources = [source]
-        expsrc = uniq_cdl(sources + [
-            catalog.entries[name].add_source(bibcode=row[-6].strip())
-        ])
-        coosrc = uniq_cdl(sources + [
-            catalog.entries[name].add_source(bibcode=row[-5].strip())
-        ])
-        dissrc = uniq_cdl(sources + [
-            catalog.entries[name].add_source(bibcode=row[-4].strip())
-        ])
+        expsrc = uniq_cdl(sources + [catalog.entries[name].add_source(bibcode=row[-6].strip())])
+        coosrc = uniq_cdl(sources + [catalog.entries[name].add_source(bibcode=row[-5].strip())])
+        dissrc = uniq_cdl(sources + [catalog.entries[name].add_source(bibcode=row[-4].strip())])
         flxsrc = uniq_cdl(sources + [
             catalog.entries[name].add_source(bibcode=row[-3].strip()),
             catalog.entries[name].add_source(bibcode=row[-2].strip())
         ])
 
-        catalog.entries[name].add_quantity(SUPERNOVA.CLAIMED_TYPE, row[1],
-                                           source)
+        if len(row[1]) > 0:
+            catalog.entries[name].add_quantity(SUPERNOVA.CLAIMED_TYPE, row[1], source)
         date = astrotime(float(row[2]), format='jd').datetime
         catalog.entries[name].add_quantity(
-            SUPERNOVA.EXPLOSION_DATE,
-            make_date_string(date.year, date.month, date.day), expsrc)
+            SUPERNOVA.EXPLOSION_DATE, make_date_string(date.year, date.month, date.day), expsrc)
         catalog.entries[name].add_quantity(
             SUPERNOVA.RA, ' '.join(row[3].split()[:3]), coosrc)
         catalog.entries[name].add_quantity(
             SUPERNOVA.DEC, ' '.join(row[3].split()[3:]), coosrc)
-        catalog.entries[name].add_quantity(SUPERNOVA.LUM_DIST, row[4], dissrc)
-        catalog.entries[name].add_quantity(SUPERNOVA.HOST, row[5], source)
-        catalog.entries[name].add_quantity(
-            SUPERNOVA.REDSHIFT,
-            row[6],
-            source,
-            e_value=row[7] if (row[7] and float(row[7]) != 0.0) else '')
+        if len(row[4]) > 0:
+            catalog.entries[name].add_quantity(SUPERNOVA.LUM_DIST, row[4], dissrc)
+        if len(row[5]) > 0:
+            catalog.entries[name].add_quantity(SUPERNOVA.HOST, row[5], source)
+        e_val = row[7] if (row[7] and float(row[7]) != 0.0) else None
+        if e_val is not None:
+            catalog.entries[name].add_quantity(SUPERNOVA.REDSHIFT, row[6], source, e_value=e_val)
         photodict = {
             PHOTOMETRY.TIME: jd_to_mjd(Decimal(row[8])),
             PHOTOMETRY.U_TIME: 'MJD',
@@ -72,10 +64,8 @@ def do_snax(catalog):
             PHOTOMETRY.U_ENERGY: 'keV',
             PHOTOMETRY.FLUX: str(Decimal('1.0e-13') * Decimal(row[11])),
             PHOTOMETRY.U_FLUX: 'ergs/s/cm^2',
-            PHOTOMETRY.E_LOWER_FLUX:
-            str(Decimal('1.0e-13') * Decimal(row[13])),
-            PHOTOMETRY.E_UPPER_FLUX:
-            str(Decimal('1.0e-13') * Decimal(row[14])),
+            PHOTOMETRY.E_LOWER_FLUX: str(Decimal('1.0e-13') * Decimal(row[13])),
+            PHOTOMETRY.E_UPPER_FLUX: str(Decimal('1.0e-13') * Decimal(row[14])),
             PHOTOMETRY.INSTRUMENT: row[9],
             PHOTOMETRY.SOURCE: flxsrc
         }
